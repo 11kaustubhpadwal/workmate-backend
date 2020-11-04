567e140c10836e9cd45d166a409823d5a35df3cd
Kaustubh Padwal <11kaustubhpadwal@gmail.com>
2020-11-04 19:57:00 +0100

    503 fixed.

--- db/connectToDb.js
+++ db/connectToDb.js
@@ -2,13 +2,23 @@ const mongoose = require("mongoose");
 
 const connectDatabase = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    });
-    console.log("DB CONNECTION SUCCESSFUL.");
+    if (process.env.NODE_ENV === "development") {
+      await mongoose.connect(process.env.MONGO_URI_DEVELOPMENT, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+      });
+      console.log("DB CONNECTION SUCCESSFUL.");
+    } else {
+      await mongoose.connect(process.env.MONGO_URI_PRODUCTION, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+      });
+      console.log("DB CONNECTION SUCCESSFUL.");
+    }
   } catch (error) {
     console.log(error);
   }
